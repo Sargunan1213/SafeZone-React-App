c9cf15cee576ba3b28c3bdc37224c43b6c62bde9
sargunan <sargunan.gurumurthy@mail.utoronto.ca>
2020-06-18 17:40:21 -0400

    Added onclick for submit

--- src/react-components/Signup/index.js
+++ src/react-components/Signup/index.js
@@ -3,15 +3,32 @@ import './index.css'
 
 class Signup extends React.Component{
 
+    state={
+        username:"",
+        usertype:"",
+        email:""
+    };
+
+    handleInputchange = event =>{
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+        console.log(name);
+        console.log(value);
+        this.setState({
+            [name]: value
+        });
+    };
+
     render(){
         return(
             <div>
-                <img src={require("./box.jpg")} class="img"/>
+                <img src={require("./box.jpg")} className="img"/>
                 <form className = "box">
                     <p className = "head"> SIGN UP </p>
-                    <input type="text" placeholder="    Username" className="input"/>
-                    <input type="text" placeholder="    User Type" className="input"/>
-                    <input type="text" placeholder="    Email" className="input"/>                    
+                    <input type="text" placeholder="    Username" className="input" name="username" onChange={this.handleInputchange}/>
+                    <input type="text" placeholder="    User Type" className="input" name="usertype" onChange={this.handleInputchange}/>
+                    <input type="text" placeholder="    Email" className="input" name="email" onChange={this.handleInputchange}/>                    
  		            <button className="submit"> SIGN UP </button>
                 </form>
             </div>
