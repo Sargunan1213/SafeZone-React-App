14875cf70aac1f5a5a1f26f194d2a297188a873e
mad120 <47588439+mad120@users.noreply.github.com>
2020-06-23 16:40:45 -0400

    Frontliner profile page
    created the profile page for the front line workers

---
+++ src/react-components/FrontlinerProfilePage/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+// import React, { Component } from 'react';
+// import './styles.css';
+
+
+/*
+Name, email, age, profile pic
+Contact info
+*/
+
+
+
+class FrontlinerProfilePage extends React.Component {
+    state = {
+        
+    };
+  
+    render() {
+        
+        const {frontlineOwner} = this.props;        
+
+            return (
+                <div className="frontlinerBasicInfo">
+
+                    <img className="frontlinerProfilePicture" src="favicon.ico"></img>
+                    <button>Change profile picture</button>
+
+                    <p>Name: {frontlineOwner["fronlinerName"]}</p>
+                    <p>Age: {frontlineOwner["fronlinerAge"]}</p>
+                    <p>Contact: {frontlineOwner["fronlinerTel"]}</p>                   
+                    <p>Email: {frontlineOwner["fronlinerEmail"]}</p>                    
+                     
+                </div>
+
+        );
+    }
+  }
+  
+  export default FrontlinerProfilePage;
