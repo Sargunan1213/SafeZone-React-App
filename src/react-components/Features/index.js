6ddb93e48410bf61143af88ba3ec970db19d56c4
Kartik-Sangwan <kartiksangwan29@gmail.com>
2020-06-24 22:24:06 -0400

    Revert "Revert "Added link to feedback router page from home check out button""
    This reverts commit 6ed9d66bd250f937265a539536878d2307974da3.

--- src/react-components/Features/index.js
+++ src/react-components/Features/index.js
@@ -20,31 +20,37 @@ class Features extends React.Component {
           img_src={feature1}
           name="Available Rooms"
           body="Check out rooms to rent near you!"
+          link=""
         />
         <Feature
           img_src={feature2}
           name="Make A Donation"
           body="Help frontline workers afford rooms!"
+          link=""
         />
         <Feature
           img_src={feature3}
           name="Live Cases"
           body="Check out number of patients effected by covid!"
+          link=""
         />
         <Feature
           img_src={feature4}
           name="Twitter Feed"
           body="View News Of patients and success stories of many recovered!"
+          link="/"
         />
         <Feature
           img_src={feature5}
           name="Give Feedback"
           body="Give your thoughts on improving our website"
+          link="/Feedback"
         />
         <Feature
           img_src={feature6}
           name="Community Chat"
           body="Chat with community members!"
+          link="/"
         />
       </div>
     );
