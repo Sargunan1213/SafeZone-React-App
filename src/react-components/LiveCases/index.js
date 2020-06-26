bff1a5464c009495003598ee8e99e496155c79b8
sargunan <sargunan.gurumurthy@mail.utoronto.ca>
2020-06-26 16:45:26 -0400

    Removed api

--- src/react-components/LiveCases/index.js
+++ src/react-components/LiveCases/index.js
@@ -4,14 +4,15 @@ class Live extends React.Component {
   render() {
     return (
       <div>
-        <iframe
+        <img src="covid.png" />
+        {/* <iframe
           src="https://public.domo.com/cards/aKg4r"
           width="100%"
           height="600"
           marginheight="0"
           marginwidth="0"
           frameborder="0"
-        ></iframe>
+        ></iframe> */}
       </div>
     );
   }
