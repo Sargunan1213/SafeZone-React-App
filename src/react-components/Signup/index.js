bccb26cd290704894963824fc65bd5cc5de35867
sargunan <sargunan.gurumurthy@mail.utoronto.ca>
2020-06-26 20:16:18 -0400

    Formatted the sign up page

--- src/react-components/Signup/index.js
+++ src/react-components/Signup/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.css";
+import { FaRegUser, FaUserTie } from "react-icons/fa";
 
 class Signup extends React.Component {
   state = {
@@ -27,59 +28,59 @@ class Signup extends React.Component {
       <div className="back">
         <form className="box">
           <p className="head"> SIGN UP </p>
-          <input
-            type="text"
-            placeholder="    Username"
-            className="input"
-            name="username"
-            onChange={this.handleInputchange}
-          />
-          {/* <input type="text" placeholder="    User Type" className="input" name="usertype" onChange={this.handleInputchange}/> */}
-          <select name="usertype" className="input">
-            <option value="Homeowner">Homeowner</option>
-            <option value="Admin">Admin</option>
-            <option defaultValue="Customer">
-              Customer
-            </option>
-          </select>
-          <input
-            type="text"
-            placeholder="    Email"
-            className="input"
-            name="email"
-            onChange={this.handleInputchange}
-          />
-          <input
-            type="password"
-            placeholder="   Password"
-            className="input"
-            name="password"
-            onChange={this.handleInputchange}
-          ></input>
-          <input
-            type="name"
-            placeholder="    Name"
-            className="input"
-            name="name"
-            onChange={this.handleInputchange}
-          ></input>
-
-          <input
-            type="number"
-            placeholder="    Age"
-            className="input"
-            name="age"
-            onChange={this.handleInputchange}
-          ></input>
-
-          <input
-            type="tel"
-            placeholder="    Telephone Number"
-            className="input"
-            name="contactNumber"
-            onChange={this.handleInputchange}
-          ></input>
-
+          <div className="box2">
+            <div className="item1">
+              <input
+                type="text"
+                placeholder="Username"
+                className="input"
+                name="username"
+                onChange={this.handleInputchange}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="input"
+                name="password"
+                onChange={this.handleInputchange}
+              ></input>
+              <select name="usertype" className="input">
+                <option value="Homeowner">Homeowner</option>
+                <option value="Admin">Admin</option>
+                <option defaultValue="Customer">Customer</option>
+              </select>
+              <input
+                type="tel"
+                placeholder="    Telephone Number"
+                className="input"
+                name="contactNumber"
+                onChange={this.handleInputchange}
+              ></input>
+            </div>
+            <div className="item2">
+              <input
+                type="name"
+                placeholder="Name"
+                className="input"
+                name="name"
+                onChange={this.handleInputchange}
+              ></input>
+              <input
+                type="text"
+                placeholder="Email"
+                className="input"
+                name="email"
+                onChange={this.handleInputchange}
+              />
+              <input
+                type="number"
+                placeholder="Age"
+                className="input"
+                name="age"
+                onChange={this.handleInputchange}
+              ></input>
+            </div>
+          </div>
           <button className="submit"> SIGN UP </button>
         </form>
       </div>
