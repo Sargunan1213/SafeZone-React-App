71ba9ef673a4fae280b041505b1940424b68fc0f
mad120 <47588439+mad120@users.noreply.github.com>
2020-06-29 14:57:53 -0400

    styling added
    styling added

--- src/react-components/FrontlinerProfilePage/index.js
+++ src/react-components/FrontlinerProfilePage/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
-// import React, { Component } from 'react';
-// import './styles.css';
-
-
-/*
-Name, email, age, profile pic
-Contact info
-*/
-
+import './styles.css';
 
 
 class FrontlinerProfilePage extends React.Component {
-    state = {
-        
+    state = {        
     };
   
     render() {
@@ -23,13 +14,15 @@ class FrontlinerProfilePage extends React.Component {
                 <div className="frontlinerBasicInfo">
 
                     <img className="frontlinerProfilePicture" src="favicon.ico" alt="frontlinerProfilePicture.jpg" ></img>
-                    <button>Change profile picture</button>
+                    <button className="frontlinerProfile-btn">Change profile picture</button>
 
-                    <p>Name: {frontlineOwner["fronlinerName"]}</p>
-                    <p>Age: {frontlineOwner["frontlinerAge"]}</p>
-                    <p>Contact: {frontlineOwner["frontlinerTel"]}</p>                   
-                    <p>Email: {frontlineOwner["frontlinerEmail"]}</p> 
-                    <p>Interested homes ids: {frontlineOwner["interest"]}</p>                   
+                    <div className="frontline_general_info">
+                        <h2>Name: {frontlineOwner["fronlinerName"]}</h2>
+                        <h2>Age: {frontlineOwner["frontlinerAge"]}</h2>
+                        <h2>Contact: {frontlineOwner["frontlinerTel"]}</h2>                   
+                        <h2>Email: {frontlineOwner["frontlinerEmail"]}</h2> 
+                        <h2>Interested homes ids: {frontlineOwner["interest"]}</h2>
+                    </div>                                       
                      
                 </div>
 
