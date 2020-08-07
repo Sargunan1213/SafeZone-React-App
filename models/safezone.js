4423c1455750c871dff23f0492b4c5d128974546
Prynciss Ng <prynciss.ng@mail.utoronto.ca>
2020-08-06 22:18:37 -0400

    fix

--- models/safezone.js
+++ models/safezone.js
@@ -25,7 +25,8 @@ const HomeSchema = new mongoose.Schema({
     validate: {
       validator: validator.isEmail,
       message: "Not valid email",
-    },
+    }
+  },
   creator: {
 		type: mongoose.Schema.Types.ObjectId,
 		required: true
@@ -64,7 +65,6 @@ const UserSchema = new mongoose.Schema({
     },
   },
   profilePic: String,
-  homes: [HomeSchema],
   type: String,
 });
 
